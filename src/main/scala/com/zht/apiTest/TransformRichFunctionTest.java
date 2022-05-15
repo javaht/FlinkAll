ecabdac16a34c02925bfb1589ee552ec8353c38d
root <java_ht@163.com>
2022-05-15 23:04:54 +0800

    Flink学习

---
+++ src/main/scala/com/zht/apiTest/TransformRichFunctionTest.java
@@ -0,0 +1,38 @@
+package com.zht.apiTest;
+
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class TransformRichFunctionTest {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStreamSource<Event> stream = env.fromElements(
+                new Event("Bob", "./cart", 2000L),
+                new Event("Alice", "./prod?id=100", 3000L)
+        );
+
+        stream.map(new MyRitchMapper()).print();
+
+        env.execute();
+    }
+
+    public static class MyRitchMapper extends RichMapFunction<Event, Integer> {
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            super.open(parameters);
+        }
+
+        @Override
+        public Integer map(Event event) throws Exception {
+            return event.url.length();
+        }
+
+        @Override
+        public void close() throws Exception {
+            super.close();
+        }
+    }
+}
---
+++ src/main/scala/com/zht/apiTest/TransformTest.java
@@ -0,0 +1,29 @@
+package com.zht.apiTest;
+
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+
+
+public class TransformTest {
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        DataStreamSource<String> ds = env.readTextFile("datas/clicks.csv");
+
+
+        ds.flatMap((String line, Collector<String> out) -> {
+             String[] fields = line.split(",");
+              out.collect(fields[0]);
+              out.collect(fields[1]);
+              out.collect(fields[2]);
+        }).returns(String.class).print();
+
+        env.execute();
+
+
+    }
+}
