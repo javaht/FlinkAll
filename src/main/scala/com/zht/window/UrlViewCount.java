11b9ecd7cbe136e2bcf2105224e8103e5d87a113
zhouhengtao <java_ht@163.com>
2022-05-20 15:55:57 +0800

    增量聚合函数和全窗口函数的应用

---
+++ src/main/scala/com/zht/window/UrlViewCount.java
@@ -0,0 +1,38 @@
+package com.zht.window;
+
+import lombok.Data;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+
+@Data
+@Getter
+@Setter
+@Generated
+public class UrlViewCount {
+    public String url;
+    public Long count;
+    public Long windowStart;
+    public Long windowEnd;
+
+    public UrlViewCount(String url, Long count, Long windowStart, Long windowEnd) {
+        this.url = url;
+        this.count = count;
+        this.windowStart = windowStart;
+        this.windowEnd = windowEnd;
+    }
+
+
+    @Override
+    public String toString() {
+        return "UrlViewCount{" +
+                "url='" + url + '\'' +
+                ", count=" + count +
+                ", windowStart=" + new Timestamp(windowStart) +
+                ", windowEnd=" + new Time(windowEnd) +
+                '}';
+    }
+}
