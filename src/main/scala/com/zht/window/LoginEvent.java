df98c9d6b969709ac190dbe459c345050df070e4
zhouhengtao <java_ht@163.com>
2022-06-01 18:18:16 +0800

    Flink学习

---
+++ src/main/scala/com/zht/window/LoginEvent.java
@@ -0,0 +1,41 @@
+package com.zht.window;
+/*
+ * @Author root
+ * @Data  2022/6/1 16:16
+ * @Description
+ * */
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+public class LoginEvent {
+    public String userId;
+    public String ipAddress;
+    public String eventType;
+    public Long timestamp;
+
+    public LoginEvent(String userId, String ipAddress, String eventType, Long timestamp) {
+        this.userId = userId;
+        this.ipAddress = ipAddress;
+        this.eventType = eventType;
+        this.timestamp = timestamp;
+    }
+
+    public LoginEvent() {}
+
+    @Override
+    public String toString() {
+        return "LoginEvent{" +
+                "userId='" + userId + '\'' +
+                ", ipAddress='" + ipAddress + '\'' +
+                ", eventType='" + eventType + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+}
+
+
+
