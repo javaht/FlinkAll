e24576fbb4292a34c68445e865faa3bdb2f8028c
zhouzhou <java_ht@163.com>
2023-05-28 22:17:32 +0800

    update

---
+++ src/main/java/zht/base/transfrom/FilterDemo.java
@@ -0,0 +1,46 @@
+package zht.base.transfrom;
+
+
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import zht.bean.WaterSensor;
+import zht.functions.FilterFunctionImpl;
+
+/**
+ * TODO
+ *
+ * @author cjp
+ * @version 1.0
+ */
+public class FilterDemo {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        DataStreamSource<WaterSensor> sensorDS = env.fromElements(
+                new WaterSensor("s1", 1L, 1),
+                new WaterSensor("s1", 11L, 11),
+                new WaterSensor("s2", 2L, 2),
+                new WaterSensor("s3", 3L, 3)
+        );
+
+        // TODO filter： true保留，false过滤掉
+//        SingleOutputStreamOperator<WaterSensor> filter = sensorDS.filter(new FilterFunction<WaterSensor>() {
+//            @Override
+//            public boolean filter(WaterSensor value) throws Exception {
+//                return "s1".equals(value.getId());
+//            }
+//        });
+
+        SingleOutputStreamOperator<WaterSensor> filter = sensorDS.filter(new FilterFunctionImpl("s1"));
+
+
+        filter.print();
+
+
+        env.execute();
+    }
+
+
+}
---
+++ src/main/java/zht/functions/MapFunctionImpl.java
@@ -0,0 +1,18 @@
+package zht.functions;
+
+
+import org.apache.flink.api.common.functions.MapFunction;
+import zht.bean.WaterSensor;
+
+/**
+ * TODO
+ *
+ * @author cjp
+ * @version 1.0
+ */
+public class MapFunctionImpl implements MapFunction<WaterSensor,String> {
+    @Override
+    public String map(WaterSensor value) throws Exception {
+        return value.getId();
+    }
+}
